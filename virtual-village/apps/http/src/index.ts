7736b7ceb919d8e65351e4671c076c6e0ddfbb9f
Mahmoud Nasser <mnasserone@gmail.com>
2024-12-03 20:10:40 +0200

    db package added

--- virtual-village/apps/http/src/index.ts
+++ virtual-village/apps/http/src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { router } from './routes/v1';
+import express from "express";
+import { router } from "./routes/v1";
 
 const app = express();
 
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use("/api/v1", router);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`)
-})
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
