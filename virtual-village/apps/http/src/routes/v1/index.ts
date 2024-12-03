325b26d442d3f34205688503a806d8c5fb83b488
Mahmoud Nasser <mnasserone@gmail.com>
2024-12-03 21:31:23 +0200

    user authentication added

--- virtual-village/apps/http/src/routes/v1/index.ts
+++ virtual-village/apps/http/src/routes/v1/index.ts
@@ -2,19 +2,104 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
+import { SigninSchema, SignupSchema } from "../../types";
+import client from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../../config";
+import { hash , compare } from "../../scrypt";
 
 export const router = Router();
 
-router.post("/signup", (req, res) => {
-  res.status(200).json({
-    message: "signup",
-  });
+router.post("/signup", async (req, res) => {
+  // check the user
+  const parsedData = SignupSchema.safeParse(req.body);
+
+  if (!parsedData.success) {
+    res.status(400).json({
+      message: "invalid data",
+    });
+
+    return;
+  }
+
+  const hashedPassword = await hash(parsedData.data.password,);
+
+  try {
+    const user = await client.user.create({
+      data: {
+        username: parsedData.data.username,
+        password: hashedPassword,
+        role: parsedData.data.type === "admin" ? "Admin" : "User",
+      },
+    });
+
+    res.status(200).json({
+      userId: user.id,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "something went wrong internal server error or user already exists",
+    });
+    console.log(error);
+    return;
+  }
 });
 
-router.post("/signin", (req, res) => {
-  res.status(200).json({
-    message: "signin",
-  });
+router.post("/signin", async (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(403).json({
+      message: "invalid data",
+    });
+    return;
+  }
+
+  try {
+    const user = await client.user.findUnique({
+      where: {
+        username: parsedData.data.username,
+      },
+    });
+
+    if (!user) {
+      res.status(403).json({
+        message: "user not found",
+      });
+      return;
+    }
+
+    const isPasswordMatch = await compare(
+      parsedData.data.password,
+      user.password
+    );
+    if (!isPasswordMatch) {
+      res.status(403).json({
+        message: "wrong password",
+      });
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        role: user.role,
+      },
+     JWT_PASSWORD
+     
+    );
+
+    res.status(200).json({
+      token: token
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "something went wrong internal server error",
+    });
+    console.log(error);
+    return
+  }
+
 });
 
 router.get("/elements", (req, res) => {});
--- virtual-village/apps/http/src/types/index.ts
+++ virtual-village/apps/http/src/types/index.ts
@@ -1,14 +1,14 @@
 import z from "zod";
 
 export const SignupSchema = z.object({
-  username: z.string().email(),
-  password: z.string().min(8),
+  username: z.string(),
+  password: z.string().min(3),
   type: z.enum(["user", "admin"]),
 });
 
 export const SigninSchema = z.object({
-  username: z.string().email(),
-  password: z.string().min(8),
+  username: z.string(),
+  password: z.string().min(3),
 });
 
 export const UpdateMetadataSchema = z.object({
